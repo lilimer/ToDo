1b6e3f97f5a090ffd0e842e89e06c893d92dcf1e
Lili <lilimer@gmail.com>
2024-08-20 18:08:51 +0200

    basic frontend

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import sqlite3
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Depends, HTTPException, Response
 import crud
 from db import init_db, get_db
 from models import Task
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
@@ -15,21 +23,26 @@ async def startup_event():
 
 @app.post('/tasks/add')
 async def add_task(task: Task, db: sqlite3.Connection = Depends(get_db)):
-    task_id = crud.add_task(db, task)
-    return Task(id=task_id,
-                task_description=task.task_description,
-                done=task.done,
-                user_id=task.user_id)
+    crud.add_task(db, task)
+    return {"Task added"}
 
 
-@app.delete('/tasks/{task_id}/delete')
-async def delete_task(task: Task, db: sqlite3.Connection = Depends(get_db)):
+@app.delete('/tasks/delete')
+async def delete_task(task_id: int, db: sqlite3.Connection = Depends(get_db)):
     cursor = db.cursor()
-    cursor.execute("SELECT 1 FROM tasks WHERE id = ?", (task.id,))
+    cursor.execute("SELECT 1 FROM tasks WHERE id = ?", (task_id,))
     if not cursor.fetchone():
         raise HTTPException(status_code=404, detail="Task does not exist")
 
-    crud.delete_task(db, task.id)
+    crud.delete_task(db, task_id)
     db.commit()
 
     return Response(content="Task deleted", status_code=202)
+
+
+@app.get('/tasks')
+async def get_tasks(db: sqlite3.Connection = Depends(get_db)):
+    cursor = db.cursor()
+    cursor.execute("SELECT id, task, done, user_id FROM tasks")
+    tasks = cursor.fetchall()
+    return [{"id": row[0], "task_description": row[1], "done": row[2], "user_id": row[3]} for row in tasks]
--- models.py
+++ models.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel
 
 
 class Task(BaseModel):
-    id: int
     task_description: str
     done: bool = False
     user_id: int = 1
